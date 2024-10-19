0946212723383141546735fbeaadd91cf756e0e1
Daniel Weber <danielcweber@users.noreply.github.com>
2024-10-19 23:50:13 +0200

    Remove NullSearchService.

--- src/BaGet.Core/Extensions/DependencyInjectionExtensions.cs
+++ src/BaGet.Core/Extensions/DependencyInjectionExtensions.cs
@@ -56,7 +56,6 @@ namespace BaGet.Core
         private static void AddBaGetServices(this IServiceCollection services)
         {
             services.TryAddSingleton<ISearchResponseBuilder, SearchResponseBuilder>();
-            services.TryAddSingleton<ISearchService, NullSearchService>();
             services.TryAddSingleton<RegistrationBuilder>();
             services.TryAddSingleton<SystemTime>();
             services.TryAddSingleton<ValidateStartupOptions>();
